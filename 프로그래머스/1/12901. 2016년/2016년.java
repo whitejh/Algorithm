80bda15afdcc29f06b6aa37427a084e853dc89d3
whitejh <56130599+whitejh@users.noreply.github.com>
2024-10-06 22:57:48 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {    
+    public String solution(int a, int b) {
+        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};        
+
+        int date = 0;
+        for (int i = 0; i < a - 1; i++) {
+            date += month[i];
+        }
+
+        date += b - 1;
+
+        return day[date % 7];
+    }
+}
