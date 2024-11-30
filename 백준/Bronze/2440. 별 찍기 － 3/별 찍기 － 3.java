b3ec2f6b27e45643cf5acdaae2f9dca5421f8a16
whitejh <56130599+whitejh@users.noreply.github.com>
2024-11-30 16:41:46 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 252 ms, Memory: 18000 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (j > i) System.out.print("");
+                else System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
