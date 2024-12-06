ebf9bf305a98ded5e1b9b3aca4d5395dbed56452
whitejh <56130599+whitejh@users.noreply.github.com>
2024-12-06 18:54:14 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 168 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int M = sc.nextInt();
+            int F = sc.nextInt();
+
+            if (M == 0 && F == 0) break;
+            System.out.println(M + F);
+        }
+    }
+}
