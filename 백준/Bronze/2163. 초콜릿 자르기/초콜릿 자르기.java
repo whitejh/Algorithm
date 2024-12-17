72f5ff26c55528e0bfa91ee7d49e67ee4b4be10d
whitejh <56130599+whitejh@users.noreply.github.com>
2024-12-17 23:14:33 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 120 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+
+        bw.write(((n1 * n2) - 1) + "\n");
+        bw.flush();
+    }
+}
