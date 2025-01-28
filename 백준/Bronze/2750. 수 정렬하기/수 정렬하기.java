c9a0dd2dfa7e6600a3f58a791222fba3db98cbc9
whitejh <56130599+whitejh@users.noreply.github.com>
2025-01-28 14:05:48 +0900

    [Bronze II] Title: 수 정렬하기, Time: 120 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,27 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Arrays;
 
-// 수 정렬하기
-// 2025/1/28 화요일 오후 1시 40분
+// API 사용
+// StringBuilder 사용
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] A = new int[N];
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - 1 - i; j++) {
-                if (A[j] > A[j + 1]) {
-                    int temp = A[j];
-                    A[j] = A[j + 1];
-                    A[j + 1] = temp;
-                }
-            }
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder(); // 
+        for (int i : arr) {
+            sb.append(i).append("\n");
         }
-        for (int i = 0; i < N; i++) {
-            System.out.println(A[i]);
-        }
+        System.out.println(sb);
     }
 }
