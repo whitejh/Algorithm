1c3e3504b9579a65a273a34819c08cbc95459e6c
whitejh <56130599+whitejh@users.noreply.github.com>
2025-01-31 14:15:31 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 2.53 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,16 +1,21 @@
 import java.util.*;
 
-class Solution {
-  public long solution(long n) {
-		String[] arr = String.valueOf(n).split("");
-		Arrays.sort(arr); // 오름차순 정렬
- 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < arr.length; i++) {
-			sb.append(arr[i]); // 문자 하나씩 합치기
-		}
-        
-		return Long.parseLong(sb.reverse().toString());
- 
-	}
-}
+ class Solution {
+            public long solution(long n) {
+                // TODO 1: 정수 n을 문자열로 변환하고 각 자릿수를 배열로 저장
+                String[] digits = String.valueOf(n).split("");
+
+                // TODO 2: 내림차순으로 정렬
+                Arrays.sort(digits, Collections.reverseOrder());
+
+                // TODO 3: 정렬된 숫자를 다시 하나의 문자열로 합침
+                StringBuilder sb = new StringBuilder();
+                for(String digit : digits) {
+                    sb.append(digit);
+                }
+
+                // TODO 4: 문자열을 long형으로 변환하여 변환
+                return Long.parseLong(sb.toString());
+
+            }
+        }
