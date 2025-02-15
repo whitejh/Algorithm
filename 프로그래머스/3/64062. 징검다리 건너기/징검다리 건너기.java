b01cdf19206d4e24335708b6ac58db1c8c1c35b6
whitejh <56130599+whitejh@users.noreply.github.com>
2025-02-15 14:04:06 +0900

    [level 3] Title: 징검다리 건너기, Time: 115.68 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,40 +1,6 @@
-import java.util.PriorityQueue; 
-
-
 class Solution {
-
-        private static class Stone {
-            int idx, val;
-
-            public Stone(int idx, int val) {
-                this.idx = idx;
-                this.val = val;
-            }
-        }
-
-        public int solution(int[] stones, int k) {
-            int answer = 0;
-
-            PriorityQueue<Stone> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2.val, o1.val));
-
-            int i;
-            for (i = 0; i < k; i++) {
-                pq.add(new Stone(i, stones[i]));
-            }
-
-            answer = pq.peek().val;
-
-            while (i < stones.length) {
-                pq.add(new Stone(i, stones[i])); // O(NlogN)
-                i++;
-
-                while (pq.peek().idx < i - k) {
-                    pq.poll(); // O(NlogN)
-                }
-
-                answer = Math.min(answer, pq.peek().val);
-            }
-
-            return answer;
-        }
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        return answer;
     }
+}
