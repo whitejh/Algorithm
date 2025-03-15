cb48777b4ea20cbb6120d4b6ad5897a13d8cc56f
whitejh <56130599+whitejh@users.noreply.github.com>
2025-03-15 13:14:54 +0900

    [Silver IV] Title: 주몽, Time: 208 ms, Memory: 18456 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// 2025/3/15 토요일 오후 12시 25분
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 6
+        int M = Integer.parseInt(br.readLine()); // 9
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            list.add(num);
+        }
+
+        // 먼저 정렬!
+        Collections.sort(list); // 1 2 3 4 5 7
+
+        // 투포인터
+        int count = 0;
+        int left = 0;
+        int right = list.size() - 1;
+
+        while (left < right) {
+            if (list.get(left) + list.get(right) < M) {
+                left++;
+            } else if (list.get(left) + list.get(right) > M) {
+                right--;
+            } else {
+                count++;
+                left++;
+                right--;
+            }
+        }
+        System.out.println(count);
+    }
+}
