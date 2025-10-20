8822e024c35068b3b478f454178645971f9d84ef
whitejh <56130599+whitejh@users.noreply.github.com>
2025-10-20 17:37:49 +0900

    [Silver II] Title: 좌표 압축, Time: 1832 ms, Memory: 267280 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,35 +1,39 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 // 2025/10/20 월요일 오후 5시
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
         int[] A = new int[N];
         int[] S = new int[N];
 
-        for(int i=0; i<N; i++) {
-            A[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
             S[i] = A[i];
         }
         Arrays.sort(S);
-        Map<Integer, Integer> myMap=new HashMap<>();
-        int index=0;
-        for(int i=0; i<N; i++) {
+        Map<Integer, Integer> myMap = new HashMap<>();
+        int index = 0;
+        for (int i = 0; i < N; i++) {
             int now = S[i];
-            if(!myMap.containsKey(now)) {
+            if (!myMap.containsKey(now)) {
                 myMap.put(now, index++);
             }
         }
 
-        for(int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             int now = A[i];
             bw.write(myMap.get(now) + " ");
         }
