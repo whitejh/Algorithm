9978195d11c57fcf4a1dee5b8d0bb5e6c3914100
whitejh <56130599+whitejh@users.noreply.github.com>
2025-10-31 14:18:06 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 14700 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,15 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+// 2025/10/31(금) 오후 2시
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N + 1];
         int[] cards = new int[N + 1];
 
-        for (int i = 1; i <=N; i++){
-            cards[i] = sc.nextInt();
-            for (int j = 1; j<=i; j++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= i; j++) {
                 dp[i] = Math.max(dp[i], dp[i - j] + cards[j]);
             }
         }
