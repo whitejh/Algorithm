6839f5875ed0e8f74ca352883e38540c5569feea
whitejh <56130599+whitejh@users.noreply.github.com>
2025-11-03 15:28:03 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 312 ms, Memory: 32896 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -3,23 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 2025/10/17 금요일 오후 7시 25분
+// 2025/11/3 월요일 오후 2시 55분
+// 투포인터
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); // 9
+        int K = Integer.parseInt(st.nextToken()); // 2
         int[] arr = new int[N];
         int[] count = new int[100001];
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken()); // 3 2 5 5 6 4 4 5 7
         }
-        int left = 0;
-        int right = 0;
-        int max = 0;
+
+        // 투포인터
+        int left = 0, right = 0;
+        int max = 0; // 최댓값
 
         for (right = 0; right < N; right++) {
             int v = arr[right];
