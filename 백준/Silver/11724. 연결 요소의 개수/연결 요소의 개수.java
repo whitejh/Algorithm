1309c266182920cd1c0eb002e8330654478c1d29
whitejh <56130599+whitejh@users.noreply.github.com>
2025-11-14 15:38:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 456 ms, Memory: 114076 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,22 +1,20 @@
 import java.io.*;
 import java.util.*;
 
-// DFS
-// 2025.5.21 수요일 오전 12시 11분
+// 2025/11/14 금요일 복습
 public class Main {
-
-    final static int MAX = 1000 + 10; // N이 가질 수 있는 최댓값
-    static boolean[][] graph;
-    static boolean[] visited; // 재방문 방지 배열
     static int N, M;
     static int answer;
-    static StringBuilder sb = new StringBuilder();
+    static boolean[][] graph;
+    static boolean[] visited;
 
-    static void dfs(int idx) {
+    public static void dfs(int idx) {
         visited[idx] = true;
-        for (int i = 1; i <= N ; i++) {
-            if(!visited[i] && graph[idx][i])
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && graph[i][idx]) {
                 dfs(i);
+            }
         }
     }
 
@@ -28,32 +26,31 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        // 1. graph에 연결 정보 채우기
-        graph = new boolean[MAX][MAX];
-        visited = new boolean[MAX];
+        graph = new boolean[N + 1][N + 1];
+        visited = new boolean[N + 1];
 
+        int x, y;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            graph[u][v] = true;
-            graph[v][u] = true;
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+
+            graph[x][y] = true;
+            graph[y][x] = true;
         }
 
-        // 2. dfs(재귀함수 호출)
+            // dfs (중요!)
         for (int i = 1; i <= N; i++) {
-            if(!visited[i]) {
+            if (!visited[i]) {
                 dfs(i);
-                answer++;
+                answer++; // 새로운 연결 요소 발견 (DFS를 시작한 횟수 = 연결 요소의 개수)
             }
         }
 
-        // 3. 출력
+
         bw.write(String.valueOf(answer));
 
         bw.close();
-        br.close();
 
     }
-
 }
