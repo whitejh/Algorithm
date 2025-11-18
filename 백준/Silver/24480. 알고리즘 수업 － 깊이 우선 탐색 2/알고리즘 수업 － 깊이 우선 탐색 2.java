b1ebddbb1246864bf01382726d04cb213e0f1d2a
whitejh <56130599+whitejh@users.noreply.github.com>
2025-11-18 15:29:54 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 960 ms, Memory: 92936 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -1,15 +1,17 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-// 2025/5/21 수요일 오후 12시 30분
+
+// 2025/11/19 화요일 오후 2시 45분
 public class Main {
+
+    static final int MAX = 100000 + 10; // 상수
 
     static ArrayList<Integer>[] graph;
     static boolean[] visited;
-
     static int N, M, R;
-    static int[] answer;
-    static int order;
+    static int order; // 순서
+    static int[] answer; // 정점 i의 방문순서
 
     public static void dfs(int idx) {
         visited[idx] = true;
@@ -18,13 +20,14 @@ public class Main {
 
         for (int i = 0; i < graph[idx].size(); i++) {
             int next = graph[idx].get(i);
+
             if (!visited[next]) {
                 dfs(next);
             }
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -33,13 +36,13 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
 
-        // 1. graph에 연결 정보 채우기
-        graph = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) {
+        // graph에 연결정보 채우기
+        graph = new ArrayList[MAX];
+        for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
-        visited = new boolean[N + 1];
-        answer = new int[N + 1];
+        visited = new boolean[MAX];
+        answer = new int[MAX];
         order = 1;
 
         for (int i = 0; i < M; i++) {
@@ -50,21 +53,18 @@ public class Main {
             graph[y].add(x);
         }
 
-        // 2. 내림차순 정렬
+        // 오름차순 정렬 (인접 정점은 오름차순으로 방문한다)
         for (int i = 1; i <= N; i++) {
-            graph[i].sort(Collections.reverseOrder());
+            Collections.sort(graph[i], Collections.reverseOrder());
         }
 
-        // 3. dfs(재귀함수 호출)
-        dfs(R);
+        dfs(R); // 시작 정점 R
 
-        // 4. 출력
         for (int i = 1; i <= N; i++) {
             bw.write(String.valueOf(answer[i]));
             bw.newLine();
         }
 
         bw.close();
-        br.close();
     }
 }
