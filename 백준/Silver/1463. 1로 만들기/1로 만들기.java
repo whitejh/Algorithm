348b4bdf19b2265c403e5818961ced7f4ac7fb8f
whitejh <56130599+whitejh@users.noreply.github.com>
2025-12-09 12:53:41 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 18184 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,28 +2,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 2025/10/1 수요일 오전 12시
+import static java.lang.Math.min;
+
+// 2025/12/9 화요일 오후 12시 30분
 public class Main {
-
-    static int N;
-    static int[] D;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        D = new int[N + 1];
+        int N = Integer.parseInt(br.readLine());
+        int[] D = new int[N + 1];
+        D[0] = 0;
         D[1] = 0;
 
         for (int i = 2; i <= N; i++) {
-            D[i] = D[i - 1] + 1;
-            if (i % 2 == 0) {
-                D[i] = Math.min(D[i], D[i / 2] + 1);
-            }
-            if (i % 3 == 0) {
-                D[i] = Math.min(D[i], D[i / 3] + 1);
-            }
+            D[i] = D[i - 1] + 1; // 1 빼는 연산
+            if (i % 2 == 0) D[i] = min(D[i], D[i / 2] + 1);
+            if (i % 3 == 0) D[i] = min(D[i], D[i / 3] + 1);
         }
+
         System.out.println(D[N]);
 
+        // D[i] : i에서 1로 만드는데 걸리는 회소 연산 횟수
     }
 }
