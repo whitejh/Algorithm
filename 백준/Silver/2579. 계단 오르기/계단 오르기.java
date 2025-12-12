950367fff59284425c3e6949a8b12af2b53cee9d
whitejh <56130599+whitejh@users.noreply.github.com>
2025-12-12 12:36:55 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -2,36 +2,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 백준 실버3
+// 2025년 12월 12일 금요일 오전 12시
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		int[] data = new int[n+1];
-		for(int i=1; i<n+1; i++) {
-			data[i]  = Integer.parseInt(br.readLine());
-		}
-		
-		// 1. 한 계단 혹은 두 계단씩 오름
-		// 2. 연속된 세 개의 계단 x (시작점 포함 x, 시작점 0)
-		// 3. 마지막 계단 필수 
-		int[] dp = new int[n+1];
-		if(n==1) {
-			dp[1] = data[1];
-		}else if(n==2) {
-			dp[2] = data[2] + data[1];
-		}else {
-			dp[1] = data[1];
-			dp[2] = data[2] + data[1];
-			dp[3] = Math.max(data[1] + data[3], data[2] + data[3]);
-			
-			for(int i=4; i<n+1; i++) {
-				// 1. 두 계단 + 한 계단 오른 경우 
-				// 2. 한 번에 두 계단 오른 경우 
-				dp[i] = Math.max(dp[i-3] + data[i] + data[i-1], dp[i-2] + data[i]);
-			}
-		}
-		System.out.println(dp[n]);
-	}
+        int[] data = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            data[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0; // 시작점
+
+        if (n == 1) {
+            dp[1] = data[1];
+        } else if (n == 2) {
+            dp[2] = data[1] + data[2];
+        } else {
+            dp[1] = data[1];
+            dp[2] = data[1] + data[2];
+            dp[3] = Math.max(dp[1] + data[3], dp[0] + data[2] + data[3]);
+
+            // 1. 두 계단 + 한 계단 오른 경우
+            // 2. 한 번에 두 계단 오른 경우
+            for (int i = 4; i <= n; i++) {
+                dp[i] = Math.max(dp[i - 2] + data[i], dp[i - 3] + data[i - 1] + data[i]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
 }
+// 1. 한 계단 혹은 두 계단씩 오름
+// 2. 연속된 세 개의 계단 x (시작점 포함 x, 시작점 0)
+// 3. 마지막 계단 필수
