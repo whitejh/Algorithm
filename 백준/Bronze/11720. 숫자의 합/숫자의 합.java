59a85a5d0f4d60d927312ed4b6b61e1b7496f9fa
whitejh <56130599+whitejh@users.noreply.github.com>
2025-12-24 18:03:37 +0900

    [Bronze IV] Title: 숫자의 합, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        // 입력값을 String형 변수 sNum에 저장한 후 char[]형 변수로 변환
-        String sNum = sc.next();
-        char[] cNum = sNum.toCharArray();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String arr = br.readLine();
+        
+        char[] c = arr.toCharArray();
+
         int sum = 0;
-        for (int i = 0; i < cNum.length; i++) {
-            sum += cNum[i] - '0';
-            // cNum[i]를 정수형으로 변환하면서 sum에 더하여 누적
-            // -'0' 또는 -48을 해주어야 입력받은 숫자 값 그대로 사용가능
+        for (char value : c) {
+            sum += value - '0';
         }
         System.out.println(sum);
     }
 }
+
