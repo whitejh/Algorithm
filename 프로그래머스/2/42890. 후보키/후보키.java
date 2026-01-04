34feb45a9bea7e4912e3c053b5826fe1e44bbdb2
whitejh <56130599+whitejh@users.noreply.github.com>
2026-01-04 17:26:38 +0900

    [level 2] Title: 후보키, Time: 6.94 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,84 +1,66 @@
 import java.util.*;
+
 class Solution {
- 
-boolean[] visited; 
-int len;
-int cnt;
-Set<String> list = new HashSet<>();
-List<String> list2= new ArrayList<>();
+    List<Set<Integer>> candicateKeys;
+    List<List<Integer>> combiList;
+    int n;
+
     public int solution(String[][] relation) {
-        int answer = 0;
-        
-        len=relation[0].length;
-        cnt=relation.length;
-        visited=new boolean[len];
-       
-        
-        for(int i=1;i<=len;i++){
-            comb(0,i,relation);           
-            unique(relation);
-            list.clear();   
-        }     
-        return list2.size();
-    }
- 
-    public void comb (int start,int r,String[][] relation){     
-        if(r==0){
-            String temp="";
-            for(int i=0;i<len;i++){
-                if(visited[i]){
-                    temp+=i;
+        n = relation[0].length;
+
+        candicateKeys = new ArrayList<>();
+        combiList = new ArrayList<>();
+
+        // 조합 생성
+        for (int i = 1; i <= n; i++) {
+            getCombi(n, 0, i, new ArrayList<>());
+        }
+
+        for (int i = 0; i < combiList.size(); i++) {
+            // 유일성
+            if (!checkU(relation, combiList.get(i))) continue;
+
+            // 최소성
+            boolean isM = true;
+            for (Set<Integer> key : candicateKeys) {
+                if (combiList.get(i).containsAll(key)) {
+                    isM = false;
+                    break;
                 }
             }
-            list.add(temp);
-        }      
-        for(int i=start;i<len;i++){
-            if(!visited[i]){
-                visited[i]=true;
-                comb(start+1,r-1,relation);
-                visited[i]=false;
+
+            if (isM) {
+                candicateKeys.add(new HashSet<>(combiList.get(i)));
             }
         }
+
+        return candicateKeys.size();
     }
-    
-    public void unique(String[][] relation){
-        //유일성 판단하기
-        for(String data:list){
-            String[] temp= data.split("");
-            int[] arr=new int[temp.length];
-            for(int i=0;i<temp.length;i++){
-                int c =Integer.parseInt(temp[i]);
-                arr[i]=c;
-             }
-            //유일성 판단하기위한 set
-            Set<String> set = new HashSet<>();
-            for(int i=0;i<cnt;i++){
-                String cdd="";
-              for(String data2:temp){
-                int c =Integer.parseInt(data2);
-                cdd+=relation[i][c];
-              }
-                set.add(cdd);
-            }
-            //만약 유일하다면 최소성 검사
-            if(set.size()==cnt){
-                boolean flag=false;
-                for(String data3:list2){
-                    int cnt=0;
-                    String[] temp3= data3.split("");         
-                    for(String data4:temp3){
-                        if(data.contains(data4)){
-                            cnt++;
-                        }
-                    }
-                    if(cnt==data3.length()){
-                        flag=true;
-                    }
-                }
-                if(!flag){
-                   list2.add(data);                             
-                }
-            }
+
+    public void getCombi(int n, int start, int max, List<Integer> curList) {
+        if (max == curList.size()) {
+            combiList.add(new ArrayList<>(curList));
+            return;
         }
+
+        for (int i = start; i < n; i++) {
+            curList.add(i);
+            getCombi(n, i + 1, max, curList);
+            curList.remove(curList.size() - 1);
+        }
+    }
+
+    public boolean checkU(String[][] relation, List<Integer> combi) {
+        Set<String> check = new HashSet<>();
+
+        for (int i = 0; i < relation.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int idx : combi) {
+                sb.append(relation[i][idx]);
+            }
+            if (!check.add(sb.toString())) return false;
+        }
+
+        return true;
     }
 }
