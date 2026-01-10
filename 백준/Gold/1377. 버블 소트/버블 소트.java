0f86fe6690f05a9dbc3e31160b2176dbf9400ba4
whitejh <56130599+whitejh@users.noreply.github.com>
2026-01-10 18:51:26 +0900

    [Gold II] Title: 버블 소트, Time: 1416 ms, Memory: 86020 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.java
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -2,43 +2,39 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        Number[] arr = new Number[N];
-        
+
+        Point[] points = new Point[N];
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            arr[i] = new Number(num, i);
+            points[i] = new Point(Integer.parseInt(br.readLine()), i);
+        }
+        Arrays.sort(points); // 숫자를 기준으로 오름차순 정렬
+
+        int max = 0;
+        for (int i = 0; i < N; i++) { // 해당 숫자의 인덱스가 몇 칸 움직였는지 계산
+           if(max < points[i].idx - i)
+            max = points[i].idx - i; // -> (이동한 횟수 - 1)번
         }
 
-        // 정렬
-        Arrays.sort(arr);
-        StringBuilder sb = new StringBuilder();
-
-        int result = 0;
-        for (int i = 0; i < N; i++) {
-            result = Math.max(result, arr[i].index - i);
-        }
-        sb.append(result + 1);
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(max + 1); // 위에서 구한 값은 (이동한 횟수 - 1)번이므로 1 더함.
     }
 
-    public static class Number implements Comparable<Number> {
-        int num;
-        int index;
-        
-        public Number(int num, int index) {
-            this.num = num;
-            this.index = index;
-        }
+}
 
-        @Override
-        public int compareTo(Number o) {
-            return this.num - o.num; // num 기준 오름차순 정렬
-        }
+class Point implements Comparable<Point> {
+    int num; // 숫자
+    int idx; // 인덱스
+
+    Point(int num, int idx) {
+        this.num = num;
+        this.idx = idx;
     }
+
+    @Override
+    public int compareTo(Point o) {
+        return num - o.num;
+    }
+
 }
