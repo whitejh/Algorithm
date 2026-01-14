838c5d976fdbc4f9d9951e968c5c2288522c1fe5
whitejh <56130599+whitejh@users.noreply.github.com>
2026-01-14 17:39:27 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 240 ms, Memory: 25280 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -1,26 +1,34 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        
-        int num = 0;
-        Set<String> set = new HashSet<>();
-        
-        for(int i = 0; i < n; i++) {
-            String s = br.readLine();
-            
-            if(s.equals("ENTER")) {
-                set.clear();
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        Set<String> nicknames = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+
+            if (line.equals("ENTER")) {
+                // 이전 세션에서 등장한 유니크 닉네임 수를 더함
+                count += nicknames.size();
+                // 새 세션 시작: Set 비우기
+                nicknames.clear();
             } else {
-                if(set.contains(s))
-                    continue;
-                set.add(s);
-                num++;
+                // 현재 세션에서 닉네임 등장(중복이면 자동 무시)
+                nicknames.add(line);
             }
         }
-        System.out.println(num);
+
+        // 마지막 세션 처리 (ENTER로 안 끝날 수 있음)
+        count += nicknames.size();
+
+        System.out.println(count);
     }
 }
