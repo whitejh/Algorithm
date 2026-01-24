b265b8c1818a3cea517b34752fd960d2a854184b
whitejh <56130599+whitejh@users.noreply.github.com>
2026-01-24 13:58:58 +0900

    [Silver II] Title: DFS와 BFS, Time: 272 ms, Memory: 20876 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,70 +1,77 @@
 import java.util.*;
 import java.io.*;
 
-// 2025/8/7 목요일 오후 2시 15분
 public class Main {
-
-    final static int MAX = 1000 + 1;
-    static boolean[][] graph;
+    static ArrayList<Integer>[] list; // 인접 리스트
     static boolean[] visited;
-    static ArrayList<Integer> queue; // bfs
     static int N, M, V;
 
     public static void dfs(int idx) {
         visited[idx] = true;
         System.out.print(idx + " ");
 
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i] && graph[idx][i])
-                dfs(i);
+        // 연결된 노드들만 탐색
+        for (int next : list[idx]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
         }
     }
 
     public static void bfs() {
-        queue = new ArrayList<>();
-        visited = new boolean[MAX];
+        Queue<Integer> queue = new LinkedList<>(); // Queue 인터페이스 권장
+        visited = new boolean[N + 1];
 
         queue.add(V);
         visited[V] = true;
 
         while (!queue.isEmpty()) {
-            int idx = queue.remove(0);
+            int idx = queue.poll();
             System.out.print(idx + " ");
 
-            for (int i = 1; i <= N; i++) {
-                if (!visited[i] && graph[idx][i]) {
-                    queue.add(i);
-                    visited[i] = true;
+            for (int next : list[idx]) {
+                if (!visited[next]) {
+                    queue.add(next);
+                    visited[next] = true;
                 }
             }
         }
     }
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         V = Integer.parseInt(st.nextToken());
 
-        // 1. graph에 연결 정보 채우기
-        graph = new boolean[MAX][MAX];
-        visited = new boolean[MAX];
+        // 1. 인접 리스트 초기화
+        list = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
 
+        // 2. 간선 정보 입력
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            graph[x][y] = true;
-            graph[y][x] = true;
+            list[x].add(y);
+            list[y].add(x);
         }
 
-        // 2. dfs(재귀함수 호출)
+        // 3. 작은 번호부터 방문하기 위해 정렬 
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(list[i]);
+        }
+
+        // DFS 실행
+        visited = new boolean[N + 1];
         dfs(V);
         System.out.println();
 
-        // 3. bfs (큐로 구현)
+        // BFS 실행
         bfs();
     }
 }
