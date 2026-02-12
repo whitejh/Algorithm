9f9c987b9c7632b9eeecd904ea2ba5793cd5a840
whitejh <56130599+whitejh@users.noreply.github.com>
2026-02-12 10:56:00 +0900

    [Silver I] Title: 문자열 교환, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.java
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -1,31 +1,49 @@
-import java.io.*;
-import java.util.*;
-import java.math.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int n = str.length();
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    char[] arr = br.readLine().toCharArray();
+        // 1. a의 총 개수(윈도우 크기) 구하기
+        int aCount = 0;
+        for (char c : str.toCharArray()) {
+            if (c == 'a') aCount++;
+        }
 
-    int cnt = 0;
+        // a가 아예 없거나 전체가 a라면 교환할 필요가 없음
+        if (aCount == 0 || aCount == n) {
+            System.out.println(0);
+            return;
+        }
 
-    for(int i =0 ;i<arr.length;i++){
-      if(arr[i] == 'a') cnt++;
+        // 2. 첫 번째 윈도우(0 ~ aCount-1)에서의 b 개수 초기 설정
+        int currentBCount = 0;
+        for (int i = 0; i < aCount; i++) {
+            if (str.charAt(i) == 'b') currentBCount++;
+        }
+
+        int minBCount = currentBCount;
+
+        // 3. 슬라이딩 윈도우 시작
+        // 원형 처리를 위해 % 연산자를 사용하면 doubled 문자열 없이도 가능
+        for (int i = 1; i < n; i++) {
+            // (1) 윈도우에서 빠지는 이전의 첫 글자가 'b'였다면 count 감소
+            if (str.charAt(i - 1) == 'b') {
+                currentBCount--;
+            }
+            // (2) 새롭게 들어오는 끝 글자가 'b'라면 count 증가
+            // 원형 연결을 위해 (i + aCount - 1) % n 인덱스 사용
+            if (str.charAt((i + aCount - 1) % n) == 'b') {
+                currentBCount++;
+            }
+            
+            minBCount = Math.min(minBCount, currentBCount);
+        }
+
+        System.out.println(minBCount);
     }
-
-    int ans = cnt;
-
-    int n = arr.length;
-
-    for(int i = 0;i<n;i++){
-      int now = 0;
-      for(int j = i;j<i+cnt;j++){
-        if(arr[j%n] == 'b') now++;
-      }
-      ans = Math.min(ans,now);
-    }
-
-    System.out.println(ans);
-  }
 }
