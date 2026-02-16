15b294ce251df7f68c4337dbcd3ac6ead0327ff9
whitejh <56130599+whitejh@users.noreply.github.com>
2026-02-16 12:21:12 +0900

    [level 3] Title: 양과 늑대, Time: 5.42 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,40 +1,38 @@
 import java.util.*;
 
 class Solution {
+    static List<Integer>[] tree;
     static int ans = 0;
-    static ArrayList<Integer> [] nodes = new ArrayList [17];
-    static int [] INFO;
     public int solution(int[] info, int[][] edges) {
-        INFO = info;
-        for(int i = 0; i < 17; i++){
-            nodes[i] = new ArrayList<>();
-        }
-        for(int i = 0; i < edges.length; i++){
-            int parent = edges[i][0];
-            int child = edges[i][1];
-            nodes[parent].add(child);
-        }
-        ArrayList<Integer> non_visited = new ArrayList<Integer>();
-        non_visited.add(0);
-        recur(0, non_visited , 0, 0);
+        int answer = 0;
+        tree = new ArrayList[info.length];
+        
+        for(int i=0; i<info.length; i++) tree[i] = new ArrayList<>();
+        for(int[] arr : edges) tree[arr[0]].add(arr[1]);
+        
+        List<Integer> next = new ArrayList<>();
+        next.add(0);
+        bt(0,0,0, next, info);
+                
         return ans;
     }
-
-    public void recur(int now, ArrayList<Integer> non_visited, int lamps, int wolfs){
-        if(INFO[now] == 0) lamps++;
-        else wolfs++;
-        if(lamps <= wolfs) return;
-        // (0) 양 최대값 저장
-        ans = Math.max(lamps, ans);
-        // (1) 자식 노드 미방문 정점에 넣기
-        non_visited.addAll(nodes[now]);
-        // (2) 미방문 정점에서 자기 자신을 빼기 
-        non_visited.remove(Integer.valueOf(now));
-        // (3) non_visited에 해당하는 정점 하나씩 방문
-        for(int i = 0; i < non_visited.size(); i++){
-            int next = non_visited.get(i);
-            ArrayList<Integer> next_nodes = new ArrayList<>(non_visited);
-            recur(next,next_nodes, lamps, wolfs);
+    
+    public void bt(int cur, int sheep, int wolf, List<Integer> next, int[] info) {
+        if(info[cur] == 0) {
+            sheep++;
+        } else {
+            wolf++;
+        }
+        
+        if(wolf >= sheep) return;
+        ans = Math.max(ans, sheep);
+        
+        List<Integer> list = new ArrayList<>(next);
+        list.remove(Integer.valueOf(cur));
+        list.addAll(tree[cur]);
+        
+        for(int n : list) {
+            bt(n, sheep, wolf, list, info);
         }
     }
 }
