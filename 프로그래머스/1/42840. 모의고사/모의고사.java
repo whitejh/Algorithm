5aaf1e1701f56e785dce0c9a53e128b4e11c1d82
whitejh <56130599+whitejh@users.noreply.github.com>
2026-02-18 22:40:48 +0900

    [level 1] Title: 모의고사, Time: 2.64 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,39 +1,20 @@
-import java.util.*; 
-
+import java.util.ArrayList;
 class Solution {
-        public int[] solution(int[] answers) {
-
-            // 수포자들의 패턴을 미리 배열에 저장
-            int[][] pattern = {
-                    {1, 2, 3, 4, 5},
-                    {2, 1, 2, 3, 2, 4, 2, 5},
-                    {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-            }; 
-
-            // 수포자들의 점수를 저장할 배열
-            int[] scores = new int[3];
-
-            // 각 수포자의 패턴과 정답이 얼마나 일치하는지 확인
-            for (int i = 0; i < answers.length; i++) {
-                for (int j = 0; j < pattern.length; j++) {
-                    if (answers[i] == pattern[j][i % pattern[j].length]) {
-                        scores[j]++;
-                    }
-                }
-            }
-
-            // 가장 높은 점수 저장
-            int maxScore = Arrays.stream(scores).max().getAsInt();
-
-            // 가장 높은 점수를 가진 수포자들의 번호를 찾아서 리스트에 담음
-            ArrayList<Integer> answer = new ArrayList<>();
-
-            for (int i = 0; i < scores.length; i++) {
-                if (scores[i] == maxScore) {
-                    answer.add(i + 1);
-                }
-            }
-
-            return answer.stream().mapToInt(Integer::intValue).toArray();
+    public int[] solution(int[] answer) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[3];
+        for(int i=0; i<answer.length; i++) {
+            if(answer[i] == a[i%a.length]) {score[0]++;}
+            if(answer[i] == b[i%b.length]) {score[1]++;}
+            if(answer[i] == c[i%c.length]) {score[2]++;}
         }
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        ArrayList<Integer> list = new ArrayList<>();
+        if(maxScore == score[0]) {list.add(1);}
+        if(maxScore == score[1]) {list.add(2);}
+        if(maxScore == score[2]) {list.add(3);}
+        return list.stream().mapToInt(i->i.intValue()).toArray();
     }
+}
