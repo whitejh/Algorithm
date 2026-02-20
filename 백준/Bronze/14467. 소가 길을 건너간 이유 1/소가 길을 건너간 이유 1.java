eb23cd24fd8d5eb2a657d0867d9a22e8e1b0de53
whitejh <56130599+whitejh@users.noreply.github.com>
2026-02-20 12:44:55 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.java
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.java
@@ -1,25 +1,28 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-// 소가 길을 건너간 이유 1
-//2025/1/11 오후 11시
-
+// 2026.2.20 금요일 오후 12:30
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] A = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-        int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int[] cow = new int[11];
+        Arrays.fill(cow, -1); // 배열에 -1로 다 채워넣기
 
-        for (int i = 0; i < N; i++) { // 관찰 이력을 탐색
-            int num = sc.nextInt();
-            int s = sc.nextInt();
-            // 처음 관찰된 소가 아닌데, 이전 위치와  관찰된 위치가 다르면
-            // 소가 이동한 것이기 때문에 이동횟수를 1 늘려준다.
-            if (A[num] != -1 && A[num] != s) {
-                count++;
+        // 처음 관찰된 소가 아닌데, 이전 위치와 관찰된 위치가 다르면
+        // 소가 이동한 것이기 때문에 이동횟수를 1 늘려준다.
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            int dir = Integer.parseInt(st.nextToken());
+
+            if (cow[num] != -1 && cow[num] != dir) {
+                cnt++;
             }
-            A[num] = s; // 마지막에는 관찰된 소의 위치를 업데이트
+            cow[num] = dir; // 마지막에는 관찰된 소의 위치를 업데이트
+
         }
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
