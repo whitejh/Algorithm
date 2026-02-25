e625dda6ec1dfe3fa1700c1891ae5e71ed61c8a2
whitejh <56130599+whitejh@users.noreply.github.com>
2026-02-25 09:59:10 +0900

    [Silver IV] Title: 제로, Time: 204 ms, Memory: 23528 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,32 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
-// 5/13 화요일
-// 스택
+// 2025/2/25 화요일 오전 9시 30분
+// 늘보코치 1주차
+// 풀이시간 : 10분
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int K = Integer.parseInt(br.readLine());
-
-        Stack<Integer> stack = new Stack<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
 
         for (int i = 0; i < K; i++) {
             int num = Integer.parseInt(br.readLine());
-
-            if (num != 0) {
+            if(num != 0) {
                 stack.push(num);
             } else {
                 stack.pop();
             }
         }
+        int total = 0;
+//        for (int sNum : stack) {
+//            total += sNum;
+//        }
 
-        int sum = 0;
-        for(int v : stack) {
-            sum += v;
+        while (!stack.isEmpty()) {
+            total += stack.pop();
         }
+        System.out.println(total);
 
-        System.out.println(sum);
     }
 }
