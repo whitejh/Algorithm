3cba7dfd8bde798e368f4d4fba8a456d628c5301
whitejh <56130599+whitejh@users.noreply.github.com>
2026-03-03 21:29:16 +0900

    [Silver II] Title: 좌표 압축, Time: 1784 ms, Memory: 209176 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,42 +1,43 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-// 2025/10/20 월요일 오후 5시
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        int[] S = new int[N];
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-            S[i] = A[i];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(S);
-        Map<Integer, Integer> myMap = new HashMap<>();
-        int index = 0;
-        for (int i = 0; i < N; i++) {
-            int now = S[i];
-            if (!myMap.containsKey(now)) {
-                myMap.put(now, index++);
+        int[] sorted = Arrays.copyOf(arr,n);
+        Arrays.sort(sorted);
+        int[] unique = Arrays.stream(sorted).distinct().toArray();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int result = lowerBound(unique, arr[i]);
+            sb.append(result).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    static int lowerBound(int[] arr, int i) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < i) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-
-        for (int i = 0; i < N; i++) {
-            int now = A[i];
-            bw.write(myMap.get(now) + " ");
-        }
-        bw.flush();
+        return left;
     }
 }
