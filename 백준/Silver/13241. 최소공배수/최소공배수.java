6d6fd410e364a8566bb04f39887327c793eb555c
whitejh <56130599+whitejh@users.noreply.github.com>
2026-03-04 15:37:04 +0900

    [Silver V] Title: 최소공배수, Time: 104 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.java
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -1,16 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+// 2026.3.4 수요일 오후 3시 18분
+// 9주차 시험
+// 실버5
+// 풀이시간 20분
+// 유클리드 호제법
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		long a = sc.nextLong();
-		long b = sc.nextLong();
-		System.out.println(a * b / gcd(a, b));
-	}
-    
-	public static long gcd(Long a, Long b)
-	{
-	    if(b == 0) return a;
-	    else return gcd(b, a % b);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(A * B / gcd(A, B));
+        System.out.println(sb);
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
 }
