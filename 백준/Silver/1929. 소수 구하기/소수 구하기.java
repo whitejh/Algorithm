5c2c0920a27ddf3c9b80d51d83472dc329393963
whitejh <56130599+whitejh@users.noreply.github.com>
2026-03-04 16:18:25 +0900

    [Silver III] Title: 소수 구하기, Time: 204 ms, Memory: 20792 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -3,13 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 2026.3.4 수요일 오후 3시 53분
-// 9주차 시험 출제유형
-// 실버3
-// 풀이시간 20분
-public class Main{
-
-    public static boolean[] isPrime; // 소수를 체크하는 배열
+public class Main {
+    public static boolean[] isPrime;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,26 +14,32 @@ public class Main{
 
         isPrime = prime(N);
 
+        // 출력을 모으기 위한 StringBuilder
+        StringBuilder sb = new StringBuilder();
         for (int i = M; i <= N; i++) {
             if (isPrime[i]) {
-                System.out.println(i);
+                sb.append(i).append("\n"); // 결과 저장
             }
         }
+        // 마지막에 한 번만 출력!
+        System.out.print(sb);
     }
 
     public static boolean[] prime(int N) {
         isPrime = new boolean[N + 1];
 
-        for (int i = 0; i < isPrime.length; i++) {
-            isPrime[i] = true; // 모든 수를 소수로 가정
+        // 0부터 N까지 초기화할 때 Arrays.fill을 쓰면 코드가 더 간결해집니다.
+        // java.util.Arrays 임포트 필요
+        for (int i = 2; i <= N; i++) {
+            isPrime[i] = true;
         }
 
-        isPrime[0] = false;
-        isPrime[1] = false;
+        // 0과 1은 소수가 아님 (기본값이 false라면 생략 가능)
+        // isPrime[0] = false; isPrime[1] = false;
 
-        // 에라토스테네스의 체
         for (int i = 2; i <= Math.sqrt(N); i++) {
             if (isPrime[i]) {
+                // i*i가 int 범위를 넘을 수 있지만 N이 100만이라서 i*i는 안전합니다.
                 for (int j = i * i; j <= N; j += i) {
                     isPrime[j] = false;
                 }
