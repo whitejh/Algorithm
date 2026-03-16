89a1bccf6ebe68d4b935d83335391a61799a4876
whitejh <56130599+whitejh@users.noreply.github.com>
2026-03-16 15:03:30 +0900

    [LeetCode #300] Longest Increasing Subsequence (Java)

--- LeetCode/Medium/300. Longest Increasing Subsequence/Solution.java
+++ LeetCode/Medium/300. Longest Increasing Subsequence/Solution.java
@@ -1,21 +1,31 @@
 import java.util.Arrays;
 import java.io.*;
 
-class Solution {
+class Solution2 {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        
-        int result = 1;
-        
-        for(int i = 1; i < nums.length; i++) {
-            for(int j = 0; j <i;j++) {
-                if(nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                }
+        int[] tails = new int[nums.length];
+        int size = 0;
+
+        for (int num : nums) {
+
+            int left = 0;
+            int right = size;
+
+            while (left < right) {
+                int mid = (left + right) / 2;
+
+                if (tails[mid] < num)
+                    left = mid + 1;
+                else
+                    right = mid;
             }
-            result = Math.max(result, dp[i]);
+
+            tails[left] = num;
+
+            if (left == size)
+                size++;
         }
-        return result;
+
+        return size;
     }
 }
