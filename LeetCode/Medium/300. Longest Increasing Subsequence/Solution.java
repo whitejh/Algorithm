d3c04a0244ce8cdc97b2c57953d6d9c67452a4df
whitejh <56130599+whitejh@users.noreply.github.com>
2026-03-16 15:04:50 +0900

    [LeetCode #300] Longest Increasing Subsequence (Java)

--- LeetCode/Medium/300. Longest Increasing Subsequence/Solution.java
+++ LeetCode/Medium/300. Longest Increasing Subsequence/Solution.java
@@ -1,31 +1,50 @@
 import java.util.Arrays;
 import java.io.*;
 
-class Solution2 {
+class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] tails = new int[nums.length];
-        int size = 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
 
-        for (int num : nums) {
+        int result = 1;
 
-            int left = 0;
-            int right = size;
-
-            while (left < right) {
-                int mid = (left + right) / 2;
-
-                if (tails[mid] < num)
-                    left = mid + 1;
-                else
-                    right = mid;
+        for(int i = 1; i < nums.length; i++) {
+            for(int j = 0; j <i;j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
             }
-
-            tails[left] = num;
-
-            if (left == size)
-                size++;
+            result = Math.max(result, dp[i]);
         }
-
-        return size;
+        return result;
     }
 }
+
+//class Solution {
+//    public int lengthOfLIS(int[] nums) {
+//        int[] tails = new int[nums.length];
+//        int size = 0;
+//
+//        for (int num : nums) {
+//
+//            int left = 0;
+//            int right = size;
+//
+//            while (left < right) {
+//                int mid = (left + right) / 2;
+//
+//                if (tails[mid] < num)
+//                    left = mid + 1;
+//                else
+//                    right = mid;
+//            }
+//
+//            tails[left] = num;
+//
+//            if (left == size)
+//                size++;
+//        }
+//
+//        return size;
+//    }
+//}
