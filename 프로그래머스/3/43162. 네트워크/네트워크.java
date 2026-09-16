346f20b5f3929fe821984206a6a35b46218e5995
whitejh <56130599+whitejh@users.noreply.github.com>
2026-09-16 17:41:05 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,23 +1,29 @@
 class Solution {
+    int n;
+    int[][] computers;
+    
     public int solution(int n, int[][] computers) {
+        this.n = n;
+        this.computers = computers;
+        
         int count = 0;
         boolean[] visited = new boolean[n];
         
         for(int i = 0; i < n; i++) {
             if(visited[i]) continue;
         
-            dfs(n, computers, visited, i);
+            dfs(visited, i);
             count++;
         }
         return count;
     }
     
-    void dfs(int n, int[][] computers, boolean[] visited, int cur) {
+    void dfs(boolean[] visited, int cur) {
         visited[cur] = true;
         
         for(int i = 0; i < n; i++) {
             if(!visited[i] && computers[cur][i] == 1) {
-                dfs(n, computers, visited, i);
+                dfs(visited, i);
             }
         }
     }
